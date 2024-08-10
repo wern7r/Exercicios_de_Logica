05ad1f93c4625feafd568a2e1d923da038229956
wern7r <150675012+wern7r@users.noreply.github.com>
2024-08-10 08:18:29 -0300

    Add_exercicies

---
+++ Logica em C#/exercicio 01/Aula3_variaveis/Aula3_variaveis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3_variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char primeiraletraNome;
+            string nome;
+            float valor;
+            double valorAlto;
+            bool primeiraLetraCmcM;
+
+            primeiraletraNome = 'W';
+            nome = "Werner";
+            valor = 0.3f;
+            valorAlto = 1212.21;
+            primeiraLetraCmcM = false;
+
+
+
+
+        }
+    }
+}
---
+++ Logica em C#/exercicio02/exercicio02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------Dados da Compra--------");
+            Console.WriteLine("Digite seu Saldo: ");
+            string saldo = Console.ReadLine();
+
+            Console.WriteLine("Digite seu Nome: ");
+            string Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu Produto: ");
+            string Produto = Console.ReadLine();
+
+            Console.WriteLine("----------------Dados da Compra----------------");
+            Console.WriteLine("Produto:" + Produto);
+
+            Console.WriteLine("Nome:" + Nome);
+
+            Console.WriteLine("Saldo:" + saldo);
+            Console.ReadLine();
+
+
+        }
+    }
+}
